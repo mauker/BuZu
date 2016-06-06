0df489211734e726ba7f31f410a1ab7b2d866c78
Ricardo Hurla <ricardo.hurla@examtime.com>
2016-06-06 16:19:10 +0100

    added viewController classes and logic for the cell

--- buzu/Controllers/Cells/BusLaneTableViewCell.swift
+++ buzu/Controllers/Cells/BusLaneTableViewCell.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2016 Ricardo Hurla. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol BusLaneTableViewCellDelegate{
+    func didTouchFavoriteForCell(cell:BusLaneTableViewCell)
+}
+
+class BusLaneTableViewCell: UITableViewCell {
+    
+    var delegate:BusLaneTableViewCellDelegate! = nil
+    
+    @IBOutlet weak var lineNumberLabel: UILabel!
+    @IBOutlet weak var lineNameLabel: UILabel!
+    @IBOutlet weak var lineDirectionImage: UIImageView!
+    @IBOutlet weak var lineFavoriteButton: UIButton!
+    
+    func setUpBusLaneCell(lineNumber:String, lineName:String, lineDirection:NSNumber, isFavorite:Bool) {
+        
+        self.lineNumberLabel.text = lineNumber;
+        self.lineNameLabel.text = lineName;
+        
+        let directionImage = lineDirection == 1 ? UIImage(named:"back_arrow"):UIImage(named:"forward_arrow")
+        
+        self.lineDirectionImage.image = directionImage
+        
+        let favImage = isFavorite == false ? UIImage(named:"star_empty"):UIImage(named:"star_full")
+        
+        self.lineFavoriteButton.setImage(favImage, forState: UIControlState.Normal)
+    }
+    
+    @IBAction func didTouchFavoriteButton(sender: UIButton) {
+        self.delegate .didTouchFavoriteForCell(self)
+    }
+}
---
+++ buzu/Controllers/FeedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeedViewController.swift
+//  buzu
+//
+//  Created by Ricardo Hurla on 06/06/2016.
+//  Copyright © 2016 Ricardo Hurla. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
